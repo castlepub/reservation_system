b8ccbf59ae0fac35bc7e94dddaca2c01df1dc01a
castlepub <dira.berlin@gmail.com>
2025-07-28 19:24:17 +0200

    Fix Railway frontend: Use dynamic API URL instead of localhost

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = window.location.origin;
 let authToken = localStorage.getItem('authToken');
 
 // DOM Elements
